97f8872e99c1cb7953a0cc2345eae86950991f46
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-04-03 20:39:30 -0300

    geting a user by id

--- src/main/java/com/delfino/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/delfino/workshopmongo/dto/UserDTO.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import com.delfino.workshopmongo.domain.User;
 
 public class UserDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
 
 	private String id;
 	private String name;
