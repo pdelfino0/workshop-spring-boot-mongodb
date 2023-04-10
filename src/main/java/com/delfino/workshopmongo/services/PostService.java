cf1ad014a3a93abaee482cbade389bf46043b41b
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-04-10 20:44:46 -0300

    geting post by id

---
+++ src/main/java/com/delfino/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.delfino.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.delfino.workshopmongo.domain.Post;
+import com.delfino.workshopmongo.repository.PostRepository;
+import com.delfino.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
